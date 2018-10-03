2b652a904167bc269fa1b8713dc83cc33a6b3279
Ann <ann.burd.9996@gmail.com>
2018-10-03 22:34:56 +0300

    fixed handle_c

--- src/handle_c.c
+++ src/handle_c.c
@@ -1,39 +1,34 @@
-///* ************************************************************************** */
-///*                                                                            */
-///*                                                        :::      ::::::::   */
-///*   handle_c.c                                         :+:      :+:    :+:   */
-///*                                                    +:+ +:+         +:+     */
-///*   By: aburdeni <marvin@42.fr>                    +#+  +:+       +#+        */
-///*                                                +#+#+#+#+#+   +#+           */
-///*   Created: 2018/08/28 17:45:03 by aburdeni          #+#    #+#             */
-///*   Updated: 2018/09/27 22:57:31 by aburdeni         ###   ########.fr       */
-///*                                                                            */
-///* ************************************************************************** */
-//
-//#include "../inc/ft_printf.h"
-// /* type % doesn't exist !!!11!1 */
-//char	*handle_c(va_list aq, t_sp *mark, char *out)
-//{
-//	char	*s;
-//	char	c;
-//	char	*arg;
-//
-//	if ((TY == '%' && (c = TY)) ||
-//		(TY == 'c' && (c = (char)va_arg(aq, int))))
-//		arg = &c;
-//	TY == 's' && (arg = va_arg(aq, char*));
-//	TY != 's' && (NLEN = 1);
-//	TY == 's' && (NLEN = PREC >= 0 ? PREC : ft_strlen(arg));
-//	if ((TY != 's' && WIDTH <= 1) || (TY == 's' && !WIDTH && PREC < 0))
-//		return (printf_joinfree(out, arg, NLEN));
-//	SIZE = NLEN > (size_t)WIDTH ? NLEN : WIDTH;
-//	s = (char*)malloc(sizeof(char) + (SIZE + 1));
-//	s[SIZE] = 0;
-//	SPC = (short)((WIDTH - (short)NLEN) < 0 ? 0 : (WIDTH - (short)NLEN));
-//	if (!MINUS && SPC)
-//		ft_memset(s, (TY == '%' && ZERO) ? '0' : ' ', (size_t)SPC);
-//	ft_memcpy(s + (MINUS ? 0 : (size_t)SPC), arg, NLEN);
-//	if (MINUS && SPC)
-//		ft_memset(s + NLEN, ' ', (size_t)SPC);
-//	return (ft_strjoinfree(out, s));
-//}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_c.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aburdeni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/28 17:45:03 by aburdeni          #+#    #+#             */
+/*   Updated: 2018/09/27 22:57:31 by aburdeni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ft_printf.h"
+
+void	handle_c(t_print *aq)
+{
+	char	c;
+	char	*arg;
+
+	if ((S.ty == '%' && (c = S.ty)) ||
+		(S.ty == 'c' && (c = (char)va_arg(aq->va, int))))
+		arg = &c;
+	S.ty == 's' && (arg = va_arg(aq->va, char*));
+	S.ty != 's' && (S.ln = 1);
+	S.ty == 's' && (S.ln = PREC >= 0 ? PREC : ft_strlen(arg));
+	if ((size_t)WIDTH <= S.ln)
+		return (pr_join(aq, arg, S.ln));
+	S.free = (short)(WIDTH - S.ln < 0 ? 0 : WIDTH - S.ln);
+	if (!S.minus && S.free)
+		pr_set(aq, (char)(S.ty == '%' && S.zero ? '0' : ' '), (size_t)S.free);
+	pr_join(aq, arg, S.ln);
+	if (S.minus && S.free)
+		pr_set(aq, ' ', (size_t)S.free);
+}
