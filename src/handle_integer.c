d668752e5dd33b8905c1b7e51031aeb443b6e5e7
aburdeni <>
2018-10-12 20:29:27 +0300

    fixed some bugs, work almost fine

--- src/handle_integer.c
+++ src/handle_integer.c
@@ -6,7 +6,7 @@
 /*   By: aburdeni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 20:10:04 by aburdeni          #+#    #+#             */
-/*   Updated: 2018/10/11 22:21:41 by aburdeni         ###   ########.fr       */
+/*   Updated: 2018/10/12 20:16:34 by aburdeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ static void	set_flag_1(t_print *aq)
 {
 	if (S.ty == 'D' || S.ty == 'O' || S.ty == 'U')
 	{
-		S.length = l;
+		if (l > S.length)
+			S.length = l;
 		S.ty = (char)ft_tolower(S.ty);
 	}
 	if (S.ty == 'b')
@@ -35,28 +36,12 @@ static void	get_i(t_print *aq, uintmax_t *ut)
 		extract_i(aq, 0, ut);
 	else
 		extract_ui(aq, ut);
-	if (!S.v && ((!S.prec && S.ty != 'o') ||
-			(S.hash || (!S.prec && (!S.hash || !S.minus)))))
-		S.ln = 0;
-	else
-		S.ln = ft_nbrulen(*ut, S.base);
-	if (S.apost && DEC)
-	{
-		S.apost = (short)(S.ln / 3 - (S.ln % 3 ? 0 : 1));
-		S.ln += S.apost;
-	}
-	else
-		S.apost = 0;
-}
-
-static void	set_flag_2(t_print *aq)
-{
 	if (!DEC)
 	{
 		S.plus = 0;
 		S.spc = 0;
 		if (S.hash &&
-		(S.ty == 'o' || (!HEX && S.prec) || (HEX && !S.v && S.prec)))
+				(S.ty == 'o' || (HEX && !S.v && S.prec)))
 			S.hash = 1;
 		else if (S.ty == 'p' || (S.hash && HEX && S.v))
 			S.hash = 2;
@@ -65,11 +50,27 @@ static void	set_flag_2(t_print *aq)
 	}
 	else
 		S.hash = 0;
+	if (!S.v && ((!S.prec && S.ty != 'o') ||
+			((S.hash && S.ty != 'p') || (!S.prec && (!S.hash || !S.minus)))))
+		S.ln = 0;
+	else
+		S.ln = ft_nbrulen(*ut, S.base);
+}
+
+static void	set_flag_2(t_print *aq)
+{
+	if (S.apost && DEC)
+	{
+		S.apost = (short)(S.ln / 3 - (S.ln % 3 ? 0 : 1));
+		S.ln += S.apost;
+	}
+	else
+		S.apost = 0;
 	S.free = S.wi;
 	!(S.ty == 'o' && S.v && S.prec > (short)(S.ln)) && (S.free -= S.hash);
 	(S.v < 0 || S.plus || S.spc) && (S.free--);
 	S.free -= S.prec > (short)(S.ln) ? S.prec : S.ln;
-	S.hash && !S.v && S.prec && (S.free++);
+	S.hash && !S.v && S.prec > 0 && (S.free++);
 	S.free < 0 && (S.free = 0);
 }
 
@@ -86,18 +87,16 @@ static void	set_format_i(t_print *aq)
 		aq->out[aq->i++] = '-';
 	else if (S.plus || S.spc)
 		aq->out[aq->i++] = (char)(S.plus ? '+' : ' ');
-	else if (S.ty == 'p')
-		pr_join_str(aq, "0x", 2);
 	else if (S.hash)
 	{
 		aq->out[aq->i++] = '0';
 		S.hash == 2 && (aq->out[aq->i++] = (char)(S.ty == 'X' ? 'X' : 'x'));
 	}
-	if (S.zero && S.prec == -2 && S.free
-		&& !((HEX || S.ty == 'o' || DEC) && S.minus))
+	if (S.zero && S.prec == -2 && S.free &&
+			!(/*(HEX || S.ty == 'o' || DEC) &&*/ S.minus))
 		pr_join(aq, '0', (size_t)S.free);
-	else if (!(HEX && S.minus) && (S.zero =
-			(short)(S.prec - S.ln - (!S.v || S.ty == 'o' ? S.hash : 0))) > 0)
+	else if ((S.zero = (short)(S.prec - S.ln -
+			(!S.v || S.ty == 'o' ? S.hash : 0))) > 0)
 		pr_join(aq, '0', (size_t)(S.zero));
 }
 
